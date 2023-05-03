77a8d054f663a3b34e27936f8c50d53eaac95a87
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-05-03 17:19:37 -0300

    feat: adding and configuring tailwindcss

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,7 @@
+import './styles/global.css'
+
 export function App() {
-  return <h1 style={{
-    fontFamily: 'Inter'
-  }}>Hello world</h1>
+  return <h1 className="text-violet-400 font-bold text-4xl">Hello world</h1>
 }
 
 export default App
