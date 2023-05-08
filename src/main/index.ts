57eee5796c5a2e98adf1b899f6dcc5eeb9d51402
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-05-08 10:06:12 -0300

    feat: configuring electron store

--- src/main/index.ts
+++ src/main/index.ts
@@ -5,6 +5,7 @@ import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
 
 import './ipc'
+import './store'
 
 function createWindow(): void {
   const mainWindow = new BrowserWindow({
