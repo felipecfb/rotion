012752479cde77def6feaa9bf5b08718f9aa255d
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-05-08 11:09:34 -0300

    feat: crud structure via ipc

--- src/shared/types/ipc.ts
+++ src/shared/types/ipc.ts
@@ -8,10 +8,28 @@ export interface Document {
  * Request
  */
 
+export interface SaveDocumentRequest extends Document {}
+
+export interface FetchDocumentRequest {
+  id: string
+}
+
+export interface DeleteDocumentRequest {
+  id: string
+}
+
 /**
  * Response
  */
 
 export interface FetchAllDocumentsResponse {
   data: Document[]
+}
+
+export interface FetchDocumentResponse {
+  data: Document
+}
+
+export interface CreateDocumentResponse {
+  data: Document
 }
