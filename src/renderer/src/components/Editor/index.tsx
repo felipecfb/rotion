2de240f0de7e80391420e7bcd85acf6bfb044300
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-05-08 14:38:01 -0300

    feat: fetch document

--- src/renderer/src/components/Editor/index.tsx
+++ src/renderer/src/components/Editor/index.tsx
@@ -6,7 +6,11 @@ import Typography from '@tiptap/extension-typography'
 import Placeholder from '@tiptap/extension-placeholder'
 import Document from '@tiptap/extension-document'
 
-export function Editor() {
+interface EditorProps {
+  content: string
+}
+
+export function Editor({ content }: EditorProps) {
   const editor = useEditor({
     extensions: [
       Document.extend({
@@ -23,8 +27,7 @@ export function Editor() {
           'before:content-[attr(data-placeholder)] before:text-gray-500 before:h-0 before:float-left before:pointer-events-none',
       }),
     ],
-    content:
-      '<h1>Back-end</h1><p>Esse é um documento que explica sobre back-end.</p>',
+    content,
     autofocus: 'end',
     editorProps: {
       attributes: {
