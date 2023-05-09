16193caeba1470247c9ffcf47b80b2189dbd9865
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-05-09 08:57:22 -0300

    feat: update document

--- src/renderer/src/components/Editor/index.tsx
+++ src/renderer/src/components/Editor/index.tsx
@@ -6,11 +6,17 @@ import Typography from '@tiptap/extension-typography'
 import Placeholder from '@tiptap/extension-placeholder'
 import Document from '@tiptap/extension-document'
 
-interface EditorProps {
+export interface OnContentUpdatedParams {
+  title: string
   content: string
 }
 
-export function Editor({ content }: EditorProps) {
+interface EditorProps {
+  content: string
+  onContentUpdated: (params: OnContentUpdatedParams) => void
+}
+
+export function Editor({ content, onContentUpdated }: EditorProps) {
   const editor = useEditor({
     extensions: [
       Document.extend({
@@ -27,6 +33,18 @@ export function Editor({ content }: EditorProps) {
           'before:content-[attr(data-placeholder)] before:text-gray-500 before:h-0 before:float-left before:pointer-events-none',
       }),
     ],
+    onUpdate: ({ editor }) => {
+      const contentRegex = /(<h1>(?<title>.+)<\/h1>(?<content>.+)?)/
+      const parsedContent = editor.getHTML().match(contentRegex)?.groups
+
+      const title = parsedContent?.title ?? 'Untitled'
+      const content = parsedContent?.content ?? ''
+
+      onContentUpdated({
+        title,
+        content,
+      })
+    },
     content,
     autofocus: 'end',
     editorProps: {
