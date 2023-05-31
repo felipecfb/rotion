b8eebb8cf8b6e2b273c5b99117c7e46a66f2e540
Felipe Bastos <felipe.bastos@kaber.com.br>
2023-05-31 09:24:44 -0300

    feat: setting for build

--- src/main/store.ts
+++ src/main/store.ts
@@ -1,11 +1,13 @@
 import Store from 'electron-store'
 import { Document } from '@shared/types/ipc'
+import { is } from '@electron-toolkit/utils'
 
 interface StoreType {
   documents: Record<string, Document>
 }
 
 export const store = new Store<StoreType>({
+  name: is.dev ? 'dev' : 'prod',
   defaults: {
     documents: {},
   },
